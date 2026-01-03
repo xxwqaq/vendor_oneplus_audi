890347b25630a0c24d411d02defec7c0289a638d
xxw <3219634304@qq.com>
2026-01-03 21:35:22 +0800

    audi: Initial vendor blobs for LineageOS 23.0

---
+++ Android.mk
@@ -0,0 +1,35 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),audi)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,ca2bd5936f5bd2443bc2a814c2a6b66cfe30a83e)
+$(call add-radio-file-sha1-checked,radio/aop.img,6ddedb8e5548177c75970c6a4aa9a12dd0077659)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,9e50d402381899c696f35f37d500c9410d19ad8b)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,5134782f94dfeceb920cd9d4cd434d6174c7de17)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,62aaa01e42805f28c2b299a53361aa30564d4c0f)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,d4470613dadc5db920c8c5e4cbb83666a15b385e)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,351392cc0f322a98a36bb87c443f63e608bf91c1)
+$(call add-radio-file-sha1-checked,radio/dsp.img,7e81146e07bb4120616a084cc932dc4836420d29)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,b08ee5de7a5621ea07c1e0d1e88ed8bfa46d54f9)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,08c35a3fff36546140b8c088b6856858fa0703bf)
+$(call add-radio-file-sha1-checked,radio/hyp.img,e45a61c24ef2d322b87b8c73fd64b27cfedd9eb8)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,a978f4c622d5da915a68afae7730f7a9b01390e5)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,d0d841e62d162c9b28e4a5b0a38caff85d48b3f5)
+$(call add-radio-file-sha1-checked,radio/modem.img,d3d3ea1da3c34c10494d1b774e3c24170aa6c40a)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,fb7d5b89feac2ef035e98e945d5f094e8d1ed95c)
+$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,e8bd546f11b435fd7afcdf4984d794acb7a4f77a)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,ec15de5d04d2977e0e03c1430df2db91d8626570)
+$(call add-radio-file-sha1-checked,radio/shrm.img,1704e5713a9f2e4ece7761b5ab3cb25fd3a98fb1)
+$(call add-radio-file-sha1-checked,radio/splash.img,fc5da5e9d4d388820d09510b3ed2ecb895fef5fe)
+$(call add-radio-file-sha1-checked,radio/tz.img,0d4f44fdfad1609a685c78ff6dd266d3dd3a36a5)
+$(call add-radio-file-sha1-checked,radio/uefi.img,4231a091569ba4897177e8a45fda02fe627c5b1b)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,caf907f1f220f8782f425bb3cbe70367ed51cd11)
+$(call add-radio-file-sha1-checked,radio/xbl.img,65d88f40360fa72cc01781db166df89eb9dc08b6)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,0705d519451efd28460195072b105cbb50a1a43a)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,6d6872de4d6a2663d888b181cd413ae92d24ee5e)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    engineering_cdt \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    oplus_sec \
+    oplusstanvbk \
+    qupfw \
+    shrm \
+    splash \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
